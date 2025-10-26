e8a0e782ec5ff50d35e4fe2603bd9c1de03556f7
kishan <kishangupta3003@gmail.com>
2025-10-26 21:41:44 +0530

    fixed some bugs

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import {Header} from "@/modules/home/header";
+import Footer from "@/modules/home/footer";
+
+export const metadata: Metadata = {
+    title:{
+        template:"VibeCode - Editor",
+        default: "Code Editor For VibeCoders - VibeCode",
+    },
+};
+
+
+export default  function HomeLayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    return (
+        <>
+        <Header />
+        <main className="z-20 relative w-full pt-20" >
+            {
+              children 
+            } 
+        </main>
+        <Footer />
+        </>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/components/providers/theme-providers"
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,7 +33,14 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
     </SessionProvider>
